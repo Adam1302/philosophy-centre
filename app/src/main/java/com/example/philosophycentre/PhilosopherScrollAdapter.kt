8ede36ad52859e257cfaec3251a1714fb88cafa8
Adam Abdulhamid <a8abdulhamid@uwaterloo.ca>
2023-02-26 20:08:11 -0500

    Add images to Philosopher Scroll List

--- app/src/main/java/com/example/philosophycentre/PhilosopherScrollAdapter.kt
+++ app/src/main/java/com/example/philosophycentre/PhilosopherScrollAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.philosophycentre
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +22,7 @@ class PhilosopherScrollAdapter(
 
     class PhilosopherScrollViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         // Add a variable for the main card view, then set an on-click listener later
+        val philosopherImageView: ImageView = view.findViewById(R.id.philosopher_item_image)
         val philosopherNameTextView: TextView = view.findViewById(R.id.philosopher_item_name)
         val philosopherInterestsTextView: TextView = view.findViewById(R.id.philosopher_item_interests)
         val philosopherItem: MaterialCardView = view.findViewById(R.id.single_philosopher_item)
@@ -35,8 +38,10 @@ class PhilosopherScrollAdapter(
 
     override fun onBindViewHolder(holder: PhilosopherScrollViewHolder, position: Int) {
         val item = dataset[position]
+        holder.philosopherImageView.setImageResource(
+            getPhilosopherImageResource(item.lastNameLower)
+        )
         holder.philosopherNameTextView.text = item.name
-        // holder.quoteAttributionView.text = context.resources.getString(item.attributionResourceId)
         holder.philosopherInterestsTextView.text = android.text.TextUtils.join(
             ",",
                     item.interests.map { branch -> branch.name }
@@ -50,6 +55,15 @@ class PhilosopherScrollAdapter(
 
     override fun getItemCount(): Int {
         return dataset.size
+    }
+
+    private fun getPhilosopherImageResource(philosopherName: String): Int {
+        val imageName: String = "philosopher_$philosopherName"
+        return context.resources.getIdentifier(
+            imageName,
+            "drawable",
+            context.packageName
+        )
     }
 
     companion object {

047ff4a78404b05047e41ff2d68332be66601cad
Adam Abdulhamid <adam13132002@hotmail.com>
2023-03-05 12:30:23 -0500

    Adding philosophers 400 BC to 1650 CE

--- app/src/main/java/com/example/philosophycentre/dataclasses/PhilosophersData.kt
+++ app/src/main/java/com/example/philosophycentre/dataclasses/PhilosophersData.kt
@@ -5,25 +5,11 @@ import com.example.philosophycentre.model.PhilosophyBranch
 
 val philosopherList : List<Philosopher> = listOf(
     Philosopher(
-        "Friedrich Nietzsche",
-        listOf<PhilosophyBranch>(
-            philosophyBranchMap["Ethics"]!!,
-            philosophyBranchMap["Aesthetics"]!!,
-            philosophyBranchMap["Axiology"]!!
-        )
-    ),
-    Philosopher(
         "Socrates",
         listOf<PhilosophyBranch>(
             philosophyBranchMap["Ethics"]!!,
             philosophyBranchMap["Epistemology"]!!,
             philosophyBranchMap["Metaphysics"]!!
-        )
-    ),
-    Philosopher(
-        "Voltaire",
-        listOf<PhilosophyBranch>(
-            philosophyBranchMap["Political Philosophy"]!!
         )
     ),
     Philosopher(
@@ -37,10 +23,102 @@ val philosopherList : List<Philosopher> = listOf(
         )
     ),
     Philosopher(
+        "Aristotle",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Logic"]!!,
+            philosophyBranchMap["Epistemology"]!!,
+            philosophyBranchMap["Metaphysics"]!!,
+            philosophyBranchMap["Axiology"]!!,
+            philosophyBranchMap["Aesthetics"]!!,
+            philosophyBranchMap["Political Philosophy"]!!
+        )
+    ),
+    Philosopher(
+        "Jesus Christ",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Metaphysics"]!!,
+            philosophyBranchMap["Axiology"]!!
+        )
+    ),
+    Philosopher(
+        "Marcus Aurelius",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Axiology"]!!
+        )
+    ),
+    Philosopher(
         "Boethius",
         listOf<PhilosophyBranch>(
             philosophyBranchMap["Ethics"]!!,
             philosophyBranchMap["Epistemology"]!!
         )
-    )
+    ),
+    Philosopher(
+        "Al-Kindi",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Logic"]!!,
+            philosophyBranchMap["Metaphysics"]!!
+        )
+    ),
+    Philosopher(
+        "Thomas Aquinas",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Logic"]!!,
+            philosophyBranchMap["Metaphysics"]!!,
+            philosophyBranchMap["Epistemology"]!!
+        )
+    ),
+    Philosopher(
+        "Michel De Montaigne",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Axiology"]!!
+        )
+    ),
+    Philosopher(
+        "Francis Bacon",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Axiology"]!!
+        )
+    ),
+    Philosopher(
+        "Thomas Hobbes",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Political Philosophy"]!!,
+            philosophyBranchMap["Ethics"]!!
+        )
+    ),
+    Philosopher(
+        "Blaise Pascal",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Logic"]!!,
+            philosophyBranchMap["Epistemology"]!!
+        )
+    ),
+    Philosopher(
+        "Baruch Spinoza",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Metaphysics"]!!,
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Epistemology"]!!
+        )
+    ),
+    Philosopher(
+        "Voltaire",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Political Philosophy"]!!
+        )
+    ),
+    Philosopher(
+        "Friedrich Nietzsche",
+        listOf<PhilosophyBranch>(
+            philosophyBranchMap["Ethics"]!!,
+            philosophyBranchMap["Aesthetics"]!!,
+            philosophyBranchMap["Axiology"]!!
+        )
+    ),
 )
